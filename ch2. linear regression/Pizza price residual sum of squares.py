96dc32532b187b6543c6d0d0f3bcfea1e043f806
soultka <soultka>
2017-12-29 20:05:29 +0900

    Residual sum of squares

---
+++ ch2. linear regression/Pizza price residual sum of squares.py
@@ -0,0 +1,10 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+# Training data
+X = [[6], [8], [10], [14],   [18]]
+y = [[7], [9], [13], [17.5], [18]]
+# Create and fit the model
+model = LinearRegression()
+model.fit(X,y)
+print('Residual sum of squares: %.2f' % np.mean((model.predict(X)-y) ** 2))
+      
