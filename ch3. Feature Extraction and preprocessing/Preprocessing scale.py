19b09ec1d3b2c55dd9b96908a445f7da9eade926
soultka <soultka>
2018-01-05 20:52:07 +0900

    ch3

---
+++ ch3. Feature Extraction and preprocessing/Preprocessing scale.py
@@ -0,0 +1,10 @@
+from sklearn import preprocessing
+import numpy as np
+X = np.array([
+    [0., 0., 5., 13., 9., 1.],
+    [0., 0., 13., 15., 10., 15.],
+    [0., 3., 15., 2., 0., 11.]
+])
+preprocessed = preprocessing.scale(X)
+print(preprocessed) 
+print(np.mean(preprocessed[0]))
---
+++ ch3. Feature Extraction and preprocessing/SURF descripter.py
@@ -0,0 +1,6 @@
+import mahotas as mh
+from mahotas.features import surf
+
+image = mh.imread('mandrill.png', as_grey=True)
+print('The first SURF descriptor:\n', surf.surf(image)[0])
+print ('Extracted %s SURF descriptors' % len(surf.surf(image)))
